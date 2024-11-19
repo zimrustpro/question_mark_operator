d4567567a13561b3c8d3b0dfe5fdf18ab2f0bab3
blessedsibanda263 <blessedsibanda263@gmail.com>
2024-11-19 08:15:20 +0200

    The ? operator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::num::ParseIntError;
+
+fn parse_and_log_str(input: &str) -> Result<i32, ParseIntError> {
+    let parsed_number = input.parse::<i32>()?;
+    println!("Number parsed successfully into {parsed_number}");
+    Ok(parsed_number)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let str_vec = vec!["Seven", "8", "9.0", "nice", "6060"];
+    for item in str_vec {
+        let parsed = parse_and_log_str(item);
+        println!("Result {:?}", parsed);
+    }
 }
