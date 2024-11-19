7b6ed77753f1e6f1dbb494df404a977ca886e922
blessedsibanda263 <blessedsibanda263@gmail.com>
2024-11-19 08:21:08 +0200

    Use ? operator inside main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 use std::num::ParseIntError;
 
-fn parse_and_log_str(input: &str) -> Result<i32, ParseIntError> {
-    let parsed_number = input.parse::<i32>()?;
-    println!("Number parsed successfully into {parsed_number}");
-    Ok(parsed_number)
-}
-
-fn main() {
-    let str_vec = vec!["Seven", "8", "9.0", "nice", "6060"];
-    for item in str_vec {
-        let parsed = parse_and_log_str(item);
-        println!("Result {:?}", parsed);
+fn main() -> Result<(), ParseIntError> {
+    for item in vec!["89", "8", "9.0", "eleven", "6060"] {
+        let parsed = item.parse::<i32>()?;
+        println!("{parsed}")
     }
+    Ok(())
 }
